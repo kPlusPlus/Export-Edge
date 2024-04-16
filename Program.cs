b9d39ebf91705b77473362cc3a9d93c8463c0714
Kresimir Ivkovic <kresimir.ivkovic@supracontrol.com>
2024-04-16 23:45:28 +0200

    export from Firefox, not so all

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using Microsoft.Data.Sqlite;
 
 class Program
 {
@@ -33,5 +34,47 @@ class Program
             Console.WriteLine("Edge bookmarks file not found.");
             Console.ReadLine();
         }
+
+        ExportFirefox();
     }
+
+
+    public static void ExportFirefox()
+    {
+        string firefoxProfilePath = @"C:\Users\username\AppData\Roaming\Mozilla\Firefox\Profiles\your_profile.default";
+        firefoxProfilePath = @"c:\Users\kresi\AppData\Roaming\Mozilla\Firefox\Profiles\nyk29heu.default-release-1697969231492\";
+
+        string databasePath = System.IO.Path.Combine(firefoxProfilePath, "places.sqlite");
+
+        // Connection string for SQLite
+        string connectionString = $"Data Source={databasePath};";
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            // Query to select bookmarks
+            string query = "SELECT url, title FROM moz_bookmarks b " +
+                           "JOIN moz_places p ON b.fk = p.id " +
+                           "WHERE b.type = 1 AND b.parent != 4;"; // Type 1 represents bookmarks, parent 4 is the Bookmarks Menu
+
+            using (var command = new Microsoft.Data.Sqlite.SqliteCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string url = reader["url"].ToString();
+                        string title = reader["title"].ToString();
+
+                        Console.WriteLine($"Title: {title}, URL: {url}");
+
+                        // Here you can export the bookmarks to a desired format
+                        // For example, you can write them to a JSON file
+                    }
+                }
+            }
+        }
+    }
+
 }
